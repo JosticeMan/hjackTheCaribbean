15d432cf2b42375c4dec51c4c99a6e09d10cdf41
BT_1N3_16 <BT_1N3_16@BT_1N3_16.bktech.local>
2017-11-01 11:47:17 -0400

    yes

--- hjackTheCarribean/src/battleshipJYSK/FrontEndSunnyK.java
+++ hjackTheCarribean/src/battleshipJYSK/FrontEndSunnyK.java
@@ -5,7 +5,6 @@ public class FrontEndSunnyK {
 	//Shows both player and boss' fields
 	public void displayMap()
 	{
-		private String m = "";
 		private String[][] playerMap = BackEndJustinY.getThePlayerGameBoard();
 		for(int i = 0; i < )
 	}
