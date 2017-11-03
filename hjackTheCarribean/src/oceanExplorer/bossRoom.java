011eaa78864651b9d391b6d84c0bddc6d9ede7f7
BT_1N3_05 <BT_1N3_05@BT_1N3_05.bktech.local>
2017-11-03 11:20:27 -0400

    Retrieved classnotes from master and added new class

---
+++ hjackTheCarribean/src/oceanExplorer/bossRoom.java
@@ -0,0 +1,10 @@
+package oceanExplorer;
+
+public class bossRoom extends NPCRoom {
+
+	public bossRoom(String description) {
+		super(description);
+		// TODO Auto-generated constructor stub
+	}
+
+}
