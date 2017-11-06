03af41d3a8ac33c01ded242f333de49281ac0207
BT_1N3_16 <BT_1N3_16@BT_1N3_16.bktech.local>
2017-11-06 11:45:10 -0500

    Somewhat done NPC?

--- hjackTheCarribean/src/oceanExplorer/SunnyNPC.java
+++ hjackTheCarribean/src/oceanExplorer/SunnyNPC.java
@@ -5,13 +5,22 @@ import battleshipJYSK.*;
 public class SunnyNPC extends NPC {
 
 	
+	/*
+	 They're as follow: (Their code will be handled by my backend class in the battelship package
+	 BoinkRader - Gives the player a general idea of where one of the opponent's ship is. 
+	 CriticalMissile - Sets a missile off that will guarantee a hit on a boat in a turn but the user cannot do anything in that time. 
+    Stormcaller - The opponent's battleships are surrounded by bad weather and unable to make a player for one turn. 
+	*/
+	
 	private String[] powerUps;
 	private String[] responses;
 	
 	public SunnyNPC() {
 		// TODO Auto-generated constructor stub
-		String[] powerUpList = {};
-		powerUps = powerUpList;
+		String[] powerUpListDesc = {"The BoinkRadar is a power up that shows the general location of an enemy's ship! Arrr!",
+									"CriticalMissile it a power up that gives the player a guarenteed hit on one of the enemy's ships!",
+									"Stormcaller creates a massive storm around the enemy, making them unable to do anything for one turn!"};
+		powerUps = powerUpListDesc;
 		
 		String[] positiveResp = {"yes", "sure", "okay"};
 		String[] negativeResp = {"no", "nope", "nah"};
@@ -27,8 +36,8 @@ public class SunnyNPC extends NPC {
 	public void interact() {
 		CaveExplorer.print("Argghh! Did you know there are multiple power ups? Would you like to hear some?");
 		String response = CaveExplorer.in.nextLine();
-		while(!response.equalsIgnoreCase("yes")) {
-		
+		while(!response.equalsIgnoreCase("bye")) {
+			CaveExplorer.print(powerUps[(int) (Math.random()*powerUps.length)]);
 			response = CaveExplorer.in.nextLine();
 		}
 		CaveExplorer.print("Well, that was fun. Later!");
