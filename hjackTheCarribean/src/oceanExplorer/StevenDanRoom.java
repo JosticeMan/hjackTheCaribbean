d36267fb19ef00f01e20dbd19009e3255612001d
personalaccount12345 <stevenlin.lee@gmail.com>
2017-11-26 23:32:46 -0500

    a

--- hjackTheCarribean/src/oceanExplorer/StevenDanRoom.java
+++ hjackTheCarribean/src/oceanExplorer/StevenDanRoom.java
@@ -4,17 +4,31 @@ import rpgDDSL.DanielFrontend;
 
 public class StevenDanRoom extends CaveRoom {
 	
+	public boolean visited;
 	
 	public StevenDanRoom(String description) {
 		super(description);
+		visited=false;
 	}
-	public void performAction(int direction) {
+	public void interpretInput(String input) {
 		fight();
+		input=CaveExplorer.in.nextLine();
+		super.interpretInput(input);
 		
 	}
 	public void fight() {
 		DanielFrontend a=new DanielFrontend(CaveExplorer.inventory.getBeginningShip(),CaveExplorer.level);
 		a.play();
+		if(a.isWon()) {
+			visited=true;
+		}
+	}
+	public String getContents() {
+		if(!visited) {
+			return "E";
+		}
+		return super.getContents();
+		
 	}
 
 }
