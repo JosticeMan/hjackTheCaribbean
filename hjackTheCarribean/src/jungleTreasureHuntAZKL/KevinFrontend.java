37202c252807472fd331f98dd19d171376492b41
Andrew <az@192.168.1.144>
2017-11-27 19:46:23 -0500

    single line - CaveExplorer.in.nextLine() for the input getter

--- hjackTheCarribean/src/jungleTreasureHuntAZKL/KevinFrontend.java
+++ hjackTheCarribean/src/jungleTreasureHuntAZKL/KevinFrontend.java
@@ -44,7 +44,7 @@ public class KevinFrontend implements AndrewSupport {
 	}
 	
 	public static String getInput() {
-		return inputSource.nextLine();
+		return CaveExplorer.in.nextLine();
 	}
 	
 	public KevinFrontend() {
